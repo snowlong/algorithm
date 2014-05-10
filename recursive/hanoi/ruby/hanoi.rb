8926fff2d143bc7cb329002ed903c94c71c9d78c
snowlong <kobu.atama@gmail.com>
2014-05-10 22:02:58 +0900

    Add tower of hanoi in ruby

---
+++ recursive/hanoi/ruby/hanoi.rb
@@ -0,0 +1,34 @@
+# 問題
+# 3本の塔がある。
+# 1本の塔には円盤がn枚置かれている。
+# 円盤はサイズ順になっていて上のものほど小さい。
+# この円盤を他の円盤にそっくり移し変えたい。
+
+# 参考
+# 1枚の円盤を == で表すと以下のようになる。
+#     ==
+#    ====
+#   ======
+
+# ルール
+# ・1回に1枚の円盤しか動かせない
+# ・ある棒の一番上の円盤を、他の棒の円盤に移動させる
+# ・小さい円盤の上に移動させてはいけない
+def hanoi(n, from, to, aux)
+  # 円盤が1枚であれば戻す
+  if n == 1
+    p "Move disk 1 from peg #{from} to peg #{to}" if (n == 1) 
+    return
+  end
+
+  # n-1枚目をauxへ、n枚目をtoへ移動させる
+  hanoi(n-1, from, aux, to)
+  # 残りの円盤をfrom からto へ
+  p "Move disk #{n} from peg #{from} to peg #{to}"
+
+  # n-1とn枚目をauxへ移動させる(n-1枚目をfrom経由で)
+  hanoi(n-1, aux, to, from)
+
+end
+
+hanoi(3, "F", "T", "A")
